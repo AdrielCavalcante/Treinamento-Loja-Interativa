355f53c3d4cb0c62fb719f49116e7a80f94c5a41
Adriel Cavalcante <72228497+AdrielCavalcante@users.noreply.github.com>
2022-08-31 20:27:17 -0300

    subindo o principal

---
+++ wp-content/themes/fiocruz/footer.php
@@ -0,0 +1,12 @@
+<footer class="site-footer">
+    <div class="container">
+        <div class="col">
+            imagens com os orgãos governamentais
+        </div>
+    </div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+
+</html>
